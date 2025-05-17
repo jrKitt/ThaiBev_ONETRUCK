c0056a4fe496bf53e75073692dae915a0f83e8ae
jrKitt <kittichdev@gmail.com>
2025-05-17 09:49:15 +0700

    feat: init projects

---
+++ expressAPI/src/main.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = use(express());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
